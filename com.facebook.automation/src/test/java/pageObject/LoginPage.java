519b9d55b28217d92cf21c6b62d1456854407b3b
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-07-27 13:53:34 -0300

    Update LoginPage.java
    Em caso de erro no login faz uma segunda tentativa.

--- com.facebook.automation/src/test/java/pageObject/LoginPage.java
+++ com.facebook.automation/src/test/java/pageObject/LoginPage.java
@@ -7,19 +7,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import useful.functions;
+
 import java.util.Map;
 
 public class LoginPage extends AbstractPage {
 
     public LoginPage(WebDriver driver) {
-        super(driver);
+        super( driver );
     }
 
-    public final HomePage loginContainer = PageFactory.initElements(driver, HomePage.class);
+    public final HomePage loginContainer = PageFactory.initElements( driver, HomePage.class );
 
     // delay set on properties file.
     public functions functions = new functions();
-    private String password = functions.getConfig("password");
+    private String password = functions.getConfig( "password" );
     private String senha;
 
     Map<String, Object> map = ImmutableMap.of(
@@ -31,36 +32,39 @@ public class LoginPage extends AbstractPage {
 
     public LoginPage login(String email) throws InterruptedException {
 
-        System.out.println(email);
-        WebElement inputBox = loginContainer.login;
-        String textInsideInputBox = inputBox.getText();
         try {
+            System.out.println( email );
+            WebElement inputBox = (WebElement) functions.find_element( By.cssSelector( (String) map.get( "login" ) ) );
+            String textInsideInputBox = inputBox.getText();
+
             if (textInsideInputBox.isEmpty()) {
-                loginContainer.login.sendKeys(email);
+                loginContainer.login.sendKeys( email );
             } else {
                 loginContainer.login.clear();
-                loginContainer.login.sendKeys(email);
+                loginContainer.login.sendKeys( email );
             }
         } catch (Exception e) {
-
-            System.out.println("Error in login try again");
-            functions.sendKeys(By.cssSelector((String) map.get("login")), "reiload");
+            // Em caso de erro faz uma segunda tentativa.
+            driver.navigate().refresh();
+            functions.alertConfirm();
+            System.out.println( "Error in login try again" );
+            functions.sendKeys( By.cssSelector( (String) map.get( "login" ) ), email );
         }
-        return new LoginPage(driver);
+        return new LoginPage( driver );
     }
 
     public LoginPage password() throws Throwable {
 
-        senha = functions.verifiedPassword(password);
+        senha = functions.verifiedPassword( password );
 
         try {
-            loginContainer.password.sendKeys(senha);
+            loginContainer.password.sendKeys( senha );
         } catch (Exception e) {
-
-            System.out.println("Error in login try again");
-            functions.sendKeys(By.cssSelector((String) map.get("password")), senha);
+            functions.alertConfirm();
+            System.out.println( "Error in login try again" );
+            functions.sendKeys( By.cssSelector( (String) map.get( "password" ) ), senha );
         }
-        return new LoginPage(driver);
+        return new LoginPage( driver );
 
     }
 
@@ -71,10 +75,10 @@ public class LoginPage extends AbstractPage {
 
             // try again if error in click default.
         } catch (Exception e) {
-            System.out.println("error in loginbutton");
-            functions.searchAndClick(By.cssSelector((String) map.get("button")));
+            System.out.println( "error in loginbutton" );
+            functions.searchAndClick( By.cssSelector( (String) map.get( "button" ) ) );
         }
-        return new LoginPage(driver);
+        return new LoginPage( driver );
     }
 
 
@@ -82,18 +86,18 @@ public class LoginPage extends AbstractPage {
 
         driver.switchTo().defaultContent();
         String user_name_site = null;
-        System.out.println("Step getText User");
+        System.out.println( "Step getText User" );
 
         try {
             user_name_site = loginContainer.user_text_css.getText();
             loginContainer.pageHome.click();
         } catch (Exception e) {
-            user_name_site = functions.refleshPage(By.id((String) map.get("menu")));
+            user_name_site = functions.refleshPage( By.id( (String) map.get( "menu" ) ) );
         }
 
-        System.out.println("user logado: " + user_name_site);
-        Assert.assertEquals(user_name, user_name_site);
-        return new LoginPage(driver);
+        System.out.println( "user logado: " + user_name_site );
+        Assert.assertEquals( user_name, user_name_site );
+        return new LoginPage( driver );
 
     }
 
@@ -110,12 +114,12 @@ public class LoginPage extends AbstractPage {
 
         // Checks the string is null or empty or only have blanks
         public static boolean isNullOrBlank(String s) {
-            return (s == null || s.trim().equals(""));
+            return (s == null || s.trim().equals( "" ));
         }
 
         // Checks the string is null or empty
         public static boolean isNullOrEmpty(String s) {
-            return (s == null || s.equals(""));
+            return (s == null || s.equals( "" ));
         }
     }
 }
